5d3b1ee7103786291626f6c1b368f1d82c9aba59
9z9z9z9z <72688414+9z9z9z9z@users.noreply.github.com>
2023-09-21 15:23:17 +0000

    lab1 is done

---
+++ lab1/src/maker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Разделяем аргументы в массив
+IFS=$'\n' read -d '' -r -a arg_array < numbers.txt
+count=0
+sum=0
+
+# Выполняем операции над аргументами
+for an_arg in "${arg_array[@]}"; do
+  sum=$((sum + an_arg))
+  count=$((count + 1))
+done
+
+# Выводим результаты
+result=$(expr "$sum" / "$count")
+echo "Average sum: $result"
+echo "Count: $count"
