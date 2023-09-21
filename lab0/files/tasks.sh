8b79e24c67a3e34a80320e047058d15c78e3e2b2
9z9z9z9z <72688414+9z9z9z9z@users.noreply.github.com>
2023-09-21 12:25:07 +0000

    добавлены файлы, созданные во время выполнения лабораторной работы

--- lab0/files/tasks.sh
+++ lab0/files/tasks.sh
@@ -7,6 +7,8 @@ mv hello.c newhello.c
 sh ../../update.sh
 gcc newhello.c -o hello_world
 ./hello_world
+cd
+cd /workspaces/os_lab_2019
 git add .
 git commit -m "добавлены файлы, созданные во время выполнения лабораторной работы"
 git push
